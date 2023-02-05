40535966f95c4b8330f951e92322cc2b48524600
Yosif Georgiev <iodesign@live.com>
2023-02-05 18:34:19 +0200

    UPDATE: add styles and animation

--- src/components/Donut.jsx
+++ src/components/Donut.jsx
@@ -1,16 +1,46 @@
-import { useLocation } from 'react-router-dom'
+import { useLocation } from "react-router-dom";
+import { motion } from "framer-motion";
 
-const Donut = ({data}) => {
-
-  const location = useLocation()
+const Donut = ({ data }) => {
+  const location = useLocation();
 
   return (
     <>
-    {location.pathname === '/menu' ? 
-    <img src={data} alt="" width={108.45} height={104.72} /> :
-    <img src={data} alt="" width={291.44} height={291.44} />
-    }
-     
+      {location.pathname === "/menu" ? (
+        <motion.img
+          animate={{ rotate: 360 }}
+          transition={{
+            scale: 1,
+            repeat: Infinity,
+            repeatType: "reverse",
+            duration: 3,
+          }}
+          initial={{
+            scale: 1,
+          }}
+          src={data}
+          alt=""
+          width={108.45}
+          height={104.72}
+        />
+      ) : (
+        <motion.img
+          animate={{ rotate: 360 }}
+          transition={{
+            scale: 1,
+            repeat: Infinity,
+            duration: 2.5,
+          }}
+          initial={{
+            scale: 1,
+          }}
+          src={data}
+          alt=""
+          width={291.44}
+          height={291.44}
+          style={{ filter: "drop-shadow(2px 2px 11px #552041b9)" }}
+        />
+      )}
     </>
   );
 };
